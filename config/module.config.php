0a89fe464080bf2f2a0939f35f72c6c9cb96ce3b
lfq618 <lfq618@gmail.com>
2013-04-25 10:42:12 +0800

    Change-Id: I05410a7de7a677ff53ea984ccc73987ed547b720

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,10 @@ return array(
     'view_manager' => array(
         'template_path_stack' => array(
             'zfcuser' => __DIR__ . '/../view',
+        		
+        		
+        		
+        		
         ),
     ),
     'controllers' => array(
